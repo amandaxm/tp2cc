b47fe59b11835c260d0fd30387c56c3ca0c44b91
Amanda Xavier Mariano <amandaxaviermariano10@gmail.com>
2023-11-27 11:52:11 -0300

    Adicionando ml e api para actions

---
+++ api/app.py
@@ -0,0 +1,48 @@
+from flask import Flask,redirect, request,url_for,jsonify,render_template,make_response,json
+import pickle
+import pandas as pd
+from flask_cors import CORS
+import os
+import time
+
+app = Flask(__name__)
+CORS(app)
+version = os.environ['version']
+
+with open("data/rules.pickle", "rb") as handle:
+    app.model = pickle.load(handle)
+
+def generate_recommendations(model, songs):
+    recommendation = set()
+    for rule in model.iterrows():
+        antecedents = rule[1]['antecedents']
+        confidence = rule[1]['confidence']
+        for musicaReq in songs:
+            if musicaReq in antecedents and confidence > 0.5:
+                recommendation.update(rule[1]['consequents'])
+                break
+
+    return list(recommendation)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/recommender', methods=['POST'])
+def recommend():
+    try:
+        data = request.get_json(force=True)
+        songs = data['songs']
+
+        recommendations = generate_recommendations(app.model, songs)
+        
+        return jsonify({
+            'playlist': recommendations,
+            'version': version,
+            'model_date': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        })
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 400
+
+if __name__ == '__main__':
+    app.run(port=32165, debug=True)
---
+++ gerador_regras/core.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+from mlxtend.preprocessing import TransactionEncoder
+import pickle
+import os
+import ssl
+import time
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+playlist_csv = os.environ['DATA']
+def salvar_regras(regras, nome_arquivo):
+    with open(nome_arquivo, 'wb') as handle:
+        pickle.dump(regras, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def preprocessar_dados(df):
+    df = df.drop(columns=['track_uri', 'album_name', 'album_uri', 'artist_name', 'artist_uri', 'duration_ms'])
+    return df.groupby('pid')['track_name'].apply(list).tolist()
+
+def treinar_e_salvar_modelo(dados, min_sup=0.07, min_conf=0.5, nome_arquivo='data/rules.pickle'):
+    while True:
+        te = TransactionEncoder()
+        te_ary = te.fit(dados).transform(dados)
+        df_encoded = pd.DataFrame(te_ary, columns=te.columns_)
+        
+        conjuntos_frequentes = apriori(df_encoded, min_support=min_sup, use_colnames=True)
+        regras = association_rules(conjuntos_frequentes, metric="confidence", min_threshold=min_conf)
+        salvar_regras(regras, nome_arquivo)
+        time.sleep(300)
+
+if __name__ == "__main__":
+    while True:
+        dados_spotify = pd.read_csv(playlist_csv)
+        dados_preprocessados = preprocessar_dados(dados_spotify)
+        treinar_e_salvar_modelo(dados_preprocessados)
